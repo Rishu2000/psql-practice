e85f49472afa35973ce4461a6ad2f5fdc274dd54
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-03 14:08:56 +0530

    Create Table in a database

--- db.sql
+++ db.sql
@@ -4,3 +4,11 @@
 -- 3. Create database CREATE DATABASE database_name
 -- 4. Go to the different database \c database_name
 -- 5. list all tables \d
+
+-- Create Table in a database
+-- CREATE tABLE products (
+--     id INT,
+--     name VARCHAR(50),
+--     price INT,
+--     on_sale boolean
+-- );
