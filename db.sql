5768e106ec13ba1a42e5ecef7a39e5bc16f0d247
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-03 14:12:11 +0530

    To see the data inside a table.

--- db.sql
+++ db.sql
@@ -5,10 +5,12 @@
 -- 4. Go to the different database \c database_name
 -- 5. list all tables \d
 
--- Create Table in a database
+-- Create Table in a database 
 -- CREATE tABLE products (
 --     id INT,
 --     name VARCHAR(50),
 --     price INT,
 --     on_sale boolean
 -- );
+
+-- To see the data inside a table -> \d table_name
