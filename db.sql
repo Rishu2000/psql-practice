9de6bcdcffd836ddef949c14b5b51df4d8d74f4e
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-03 14:13:01 +0530

    Correction.

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- In SQL Shell
--- 1. for help \?
--- 2. list databases \l
--- 3. Create database CREATE DATABASE database_name
--- 4. Go to the different database \c database_name
--- 5. list all tables \d
+-- 1. for help -> \?
+-- 2. list databases -> \l
+-- 3. Create database -> CREATE DATABASE database_name
+-- 4. Go to the different database -> \c database_name
+-- 5. list all tables -> \d
 
--- Create Table in a database 
+-- Create Table in a database ->
 -- CREATE tABLE products (
 --     id INT,
 --     name VARCHAR(50),
