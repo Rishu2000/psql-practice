d883295ec6462e5712da8dcba5a0f4d1b088e140
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-05 20:50:32 +0530

    Restful get method to get all the restaurants.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4001;
 
+app.get("/api/v1/restaurants", (req,res) => {
+    res.json({
+        status:"success",
+        data:{
+            restaurants:["Dimino's", "Pizza hut"]
+        }
+    })
+});
+
 app.listen(port, () => {
     console.log('server is listen to the port = ' + port);
 });
