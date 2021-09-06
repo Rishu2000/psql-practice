a869f86a597116aad8a47e72c4f75580e9a4aa19
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-06 11:19:01 +0530

    Secured my postgres password by using environment varriable.

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,5 @@
 const {Pool} = require('pg');
-const pool = new Pool({
-    user: 'postgres',
-  host: 'localhost',
-  database: 'yelp',
-  password: 'rishavpgsql',
-  port: 5432,
-});
+const pool = new Pool();
 
 module.exports = {
     query: (text,params) => pool.query(text, params),
