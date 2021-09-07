e7668374f12d898fcf5e58e133677669576a60d2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-07 13:22:43 +0530

    Used context provider so that I won't have to pass the restaurants List again again to every compnents.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,18 +4,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from "./router/Home"
 import Update from "./router/Update"
 import RestDetails from "./router/RestDetails"
+import { RestaurantsContextProvider } from './context/RestaurantsContext'
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/restaurants/:id/update" component={Update}/>
-          <Route exact path="/restaurants/:id" component={RestDetails}/>
-        </Switch>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/restaurants/:id/update" component={Update}/>
+            <Route exact path="/restaurants/:id" component={RestDetails}/>
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   )
 }
 
