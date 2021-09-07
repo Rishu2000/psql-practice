a34a333341da2c2ef88128b6c265f08c7492330c
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-07 10:14:21 +0530

    Implemented react-router-dom.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../styles/App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from "./router/Home"
+import Update from "./router/Update"
+import RestDetails from "./router/RestDetails"
 
 const App = () => {
   return (
-    <div>
-      Rishav
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/restaurants/:id/update" component={Update}/>
+        <Route exact path="/restaurants/:id" component={RestDetails}/>
+      </Switch>
+    </Router>
   )
 }
 
