7fcc5491ba5ee7b5514c99694073155eb3947ae2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-07 10:36:55 +0530

    Added Header to the home page.

--- client/src/components/router/Home.js
+++ client/src/components/router/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from "../homePage/Header";
 
 const Home = () => {
     return (
         <div>
-          Home  
+          <Header/>
         </div>
     )
 }
