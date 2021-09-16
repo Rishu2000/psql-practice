529d66ea97cdc0dc3ed727c30bd13af593996bd9
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-16 10:24:11 +0530

    Insted of using useContext I used get axios because that won't work if someone directly put a url.

--- client/src/components/updatePage/UpdateRestaurants.js
+++ client/src/components/updatePage/UpdateRestaurants.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "../apis/RestaurantFinder"
 import { useParams } from 'react-router-dom'
 
 const UpdateRestaurants = (props) => {
+
     const {id} = useParams();
-    // console.log(id);
+
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("");
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get(`${id}`);
+            const {location, name, price_range} = response.data.data;
+            setName(name);
+            setPriceRange(price_range);
+            setLocation(location);
+        }
+        getData();
+    },[])
+
     return (
         <div>
             <form action="" className="action">
                 <div className="form-group my-3">
                     <label htmlFor="name">Name</label>
-                    <input id="name" className="form-control" type="text"/>
+                    <input id="name" className="form-control" value={name} onChange={(e) => setName(e.target.value)}type="text"/>
                 </div>
                 <div className="form-group my-3">
                     <label htmlFor="location">Location</label>
-                    <input id="location" className="form-control" type="text"/>
+                    <input id="location" className="form-control" value={location} onChange={(e) => setLocation(e.target.value)} type="text"/>
                 </div>
                 <div className="form-group my-3">
                     <label htmlFor="price_range">Price Range</label>
-                    {/* <input id="name" className="form-control" type="text"/> */}
-                    <select className="custom-select my-1 mr-sm-2" //value={priceRange}
-                        //onChange={(e) => setPriceRange(e.target.value)}
+                    <select className="custom-select my-1 mr-sm-2" value={priceRange}
+                        onChange={(e) => setPriceRange(e.target.value)}
                         style={{width:"100%", height:"100%", padding:"5px"}}>
-                        <option disabled>Price Range</option>
+                        <option default>Price Range</option>
                         <option value="1">$</option>
                         <option value="2">$$</option>
                         <option value="3">$$$</option>
